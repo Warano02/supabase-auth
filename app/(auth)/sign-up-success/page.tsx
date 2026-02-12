bf37b88ea7b2eb5a39490126566b998659afcb98
Warano02 <carineteoi@gmail.com>
2026-02-12 10:26:00 +0100

    login work successfully

---
+++ app/(auth)/sign-up-success/page.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+        
+        <Link href={"/login"}>Back to login</Link>
+    </div>
+  )
+}
+
+export default page
